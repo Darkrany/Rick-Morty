a7894710a1c4e7d929ed25992be43b5863af9abd
Daniel <101005069+Darkrany@users.noreply.github.com>
2023-11-05 11:13:42 +0100

    css space

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Characters from "./components/Characters";
 
 function App() {
   return (
+
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <div className="main"></div>
           <Characters/>
 
 
