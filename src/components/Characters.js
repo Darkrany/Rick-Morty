17e480248d5e4c17259267f30c40fd8717086aa1
Daniel <101005069+Darkrany@users.noreply.github.com>
2023-11-05 10:15:55 +0100

    boton cargar más

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -10,7 +10,7 @@ import {useInfiniteQuery} from '@tanstack/react-query';
 
 
 // const Characters = () => {
-    
+
 
 //     const {
 //       data,
@@ -20,17 +20,17 @@ import {useInfiniteQuery} from '@tanstack/react-query';
 //       isFetching,
 //       isFetchingNextPage,
 //     } = useInfiniteQuery({queryKey: ['character'], queryFn: () => getCharacters(),
-    
+
 // });
 
 //     if (isFetching && !isFetchingNextPage) return 'Cargando...'
 
 //     if (error) return 'Ocurrio un error: ' + error.message
-    
+
 //     return (
 //         <div>
 //             {data.map((character) => (
-                
+
 //                     <p key={character.id}>{character.name}</p>
 //                 )
 //             )}
@@ -39,49 +39,45 @@ import {useInfiniteQuery} from '@tanstack/react-query';
 // };    
 
 // export default Characters;
+const Characters = () => {
+const getCharacters = async ({ pageParam }) => {
+    const res = await fetch (pageParam)
+       return res.json()
+};
 
-const getCharacters = async (pageParam = 1 ) => {
-    return await fetch(`https://rickandmortyapi.com/api/character?page=${pageParam}`)
-    .then((res) => res.json())
-    .then(data => ({ data: data.results, nextPage: data.info.next }));
-    };
-    
-    
-    
-    const Characters = () => {
-    
-    
+
     const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
     } = useInfiniteQuery({
-    queryKey: ['character'],
-    queryFn: () => getCharacters(),
-    getNextPageParam: (lastPage, pages) => {
-    return lastPage.page < 42
-    ? lastPage.page + 1
-    : undefined;
-    }
+        queryKey: ['character'],
+        queryFn: getCharacters,
+        initialPageParam: 'https://rickandmortyapi.com/api/character?page=1',
+        getNextPageParam: (lastPage) => lastPage.info.next
     });
-    
-    
+
+
     if (isFetching && !isFetchingNextPage) return 'Cargando...'
-    
+
     if (error) return 'Ocurrio un error: ' + error.message
-    
+
+
     return (
-    <div>
-    {data.pages.map((group) => (
-    group.data.map((character) =>
-    <p key={character.id}>{character.name}</p>
-    )
-    ))}
-    </div>
+        <div>
+            {
+                data.pages.map((group) =>
+                    group.results.map((character) =>
+                        <p key={character.id}>{character.name}</p>
+                    )
+                )
+            }
+            <button onClick={() =>fetchNextPage()}>Cargar más</button>
+        </div>
     );
-    };
-    
-    export default Characters;
+};
+
+export default Characters;
