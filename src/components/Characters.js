30f5af9c218546eb9e1a19a9ef5b0192fcf676d9
Satgoltratec <sat@goltratec.com>
2023-11-08 18:53:47 +0100

    css

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,56 +1,55 @@
-import React from 'react';
-import {useInfiniteQuery} from '@tanstack/react-query';
+import React from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const Characters = () => {
-const getCharacters = async ({ pageParam }) => {
-    const res = await fetch (pageParam)
-       return res.json()
-};
+  const getCharacters = async ({ pageParam }) => {
+    const res = await fetch(pageParam);
+    return res.json();
+  };
 
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["character"],
+    queryFn: getCharacters,
+    initialPageParam: "https://rickandmortyapi.com/api/character?page=1",
+    getNextPageParam: (lastPage) => lastPage.info.next,
+  });
 
-    const {
-        data,
-        error,
-        fetchNextPage,
-        hasNextPage,
-        isFetching,
-        isFetchingNextPage,
-    } = useInfiniteQuery({
-        queryKey: ['character'],
-        queryFn: getCharacters,
-        initialPageParam: 'https://rickandmortyapi.com/api/character?page=1',
-        getNextPageParam: (lastPage) => lastPage.info.next
-    });
+  if (isFetching && !isFetchingNextPage) return "Cargando...";
 
+  if (error) return "Ocurrio un error: " + error.message;
 
-    if (isFetching && !isFetchingNextPage) return 'Cargando...'
-
-    if (error) return 'Ocurrio un error: ' + error.message
-
-
-    return (
-        <>
-        <div className="card-container">
-            {
-                data.pages.map((group) =>
-                group.results.map((character) =>
-                <div className="card" key={character.name}>
-                    <div className="img-container">
-                    <img className="img" src={character.image} alt={character.image}></img>
-                    <div className ="description card">
-                    <span className="title">{character.name}</span>
-                    </div>
-                    </div>
+  return (
+    <>
+      <div className="card-container">
+        {data.pages.map((group) =>
+          group.results.map((character) => (
+            <div className="card" key={character.name}>
+              <div className="img-container">
+                <img
+                  className="img"
+                  src={character.image}
+                  alt={character.image}
+                ></img>
+                <div className="description card">
+                  <span className="title">{character.name}</span>
                 </div>
-                    
-                        )
-                )
-            }
-            
-        </div>
-        <button class="button-53" role="button" onClick={() =>fetchNextPage()}>Cargar más</button>
-        </>
-    );
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <button class="button-53" role="button" onClick={() => fetchNextPage()}>
+        Cargar más
+      </button>
+    </>
+  );
 };
 
 export default Characters;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 //react libraries
 // import { NavLink } from "react-router-dom";
-//components
+
 import logo from "../images/Rick_and_Morty_title_card.png";
-//styles
-// import "../styles/components/Header.scss";
+
 
 function Header() {
   return (
@@ -11,7 +10,6 @@ function Header() {
      
         <img src={logo} alt="Logo Rick and Morty" className="App-logo" />
 
-      <h1 className="header__title"></h1>
     </header>
   );
 }
