4ca24af666b0a0cbd255b5e0c09578477a17b373
Satgoltratec <116006255+Satgoltratec@users.noreply.github.com>
2023-11-30 17:08:12 +0100

    filtros

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState }  from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Link } from 'react-router-dom';
+import { useDebounce } from "use-debounce";
+
+const STATUS_OPTIONS = [
+  {
+    label: 'Desconocido',
+    value: ''
+  },
+  
+  {
+    label: 'Vivo',
+    value: 'alive',
+  },
+  {
+    label: 'Muerto',
+    value: 'dead',
+  },
+  {
+    label: 'Desconocido',
+    value: 'unknown',
+  },
+
+]
+
 
 
 const Characters = () => {
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
+  const [searchDebounced] = useDebounce(search, 750);
+
   const getCharacters = async ({ pageParam }) => {
-    const res = await fetch(pageParam);
+    const url = 'https://rickandmortyapi.com/api/character';
+    const res = await fetch(pageParam + `&name=${searchDebounced}&status=${status}`);
+  
+    // la api de rick y morty no devuelve un 200 cuando no hay resultados, 
+    // devuelve un 404
+    if (res.status === 404)
+      throw new Error('No hay resultados para la búsqueda!')
     return res.json();
+  };  
+
+  // const searchCharacters = async (name) => {
+  //   const res = await fetch(`https://rickandmortyapi.com/api/character?name=${name}`);
+  //   return res.json();
+  // };
+
+  const handleChange = event => {
+    setSearch(event.target.value);
   };
 
-  const searchCharacters = async (name) => {
-    const res = await fetch(`https://rickandmortyapi.com/api/character?name=${name}`);
-    return res.json();
-  };
 
   const {
     data,
@@ -22,29 +60,43 @@ const Characters = () => {
     isFetching,
     isFetchingNextPage,
   } = useInfiniteQuery({
-    queryKey: ["character"],
+    queryKey: ["character", searchDebounced, status],
     queryFn: getCharacters,
     initialPageParam: "https://rickandmortyapi.com/api/character?page=1",
     getNextPageParam: (lastPage) => lastPage.info.next,
   });
 
-  if (isFetching && !isFetchingNextPage) return "Cargando...";
+  // if (isFetching && !isFetchingNextPage) return "Cargando...";
 
   if (error) return "Ocurrio un error: " + error.message;
 
   return (
     <>
-      <form onsubmit="event.preventDefault();" role="search">
+     
 
-        <input id="search" type="search" placeholder="Busqueda de personaje..." autofocus required />
-        <button type="submit">Go</button>
-      </form>
+        <input id="text" 
+        type="search" 
+        placeholder="Busqueda de personaje..." 
+        onChange={handleChange}
+        value={search}     
+        />
 
-    <p></p>
+        <select onChange={e => {
+          const index = e.target.selectedIndex;
+
+          setStatus(STATUS_OPTIONS[index].value)
+        }}>
+          {STATUS_OPTIONS.map(o => <option value={o.value}>{o.label}</option>)}
+        </select>
+        <p>{status}</p>
+
+
+
+
 
 
       <div className="card-container">
-        {data.pages.map((group) =>
+        {data && data.pages.map((group) =>
           group.results.map((character) => (
             <Link key={character.id} to={`/details/${character.id}`}>
               <div className="card" key={character.name}>
@@ -63,9 +115,10 @@ const Characters = () => {
           ))
         )}
       </div>
-      <button className="button-53" role="button" onClick={() => fetchNextPage()}>
+
+      {hasNextPage && <button className="button-53" role="button" onClick={() => fetchNextPage()}>
         Cargar más
-      </button>
+      </button>}
     </>
   );
 };
